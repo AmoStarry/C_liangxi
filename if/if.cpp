240934f72f5be8465db55cf3a1a507333ed872fc
santigongming <1524730261@qq.com>
2023-02-27 23:09:03 +0800

    if

---
+++ if/if.cpp
@@ -0,0 +1,29 @@
+//成绩判断
+#include <stdio.h>
+int main()
+{
+    float score
+    printf("please input a score:\n");
+    scanf("%d",&score);
+    if (score < 0 ||score >100)
+    {
+        printf("请输入一个0到一百的数\N");
+    }
+    else if (score>90)
+    {
+        printf("A\N");
+    }
+    else if (score>80)
+    {
+        printf("B\n");
+    }
+    else if (score>70)
+    {
+        printf("B\n");
+    }
+    else if (score>60)
+    {
+        printf("及格\n");
+    }
+    return 0;
+}
